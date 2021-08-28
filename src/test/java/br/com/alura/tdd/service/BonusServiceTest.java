0fe4b3268fe38d5205fc91c42533fa322d6d8dda
Matheus Lot Rizzo <matheus.l.rizzo@gmail.com>
2021-08-28 13:57:09 -0300

    Testes TDD e Junit

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,50 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+class BonusServiceTest {
+
+	@Test
+	void bonusZeroPoisSalarioMuitoAlto() {
+		
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(
+							new Funcionario("Matheus", 
+								LocalDate.now(),
+								new BigDecimal("25000")));
+		
+		assertEquals(new BigDecimal("0.00"), bonus);
+	}
+	
+	@Test
+	void bonus10PorcentodDoSalario() {
+		
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(
+							new Funcionario("Matheus", 
+								LocalDate.now(),
+								new BigDecimal("2500")));
+		
+		assertEquals(new BigDecimal("250.00"), bonus);
+	}
+	
+	@Test
+	void bonusIgualA1000() {
+		
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(
+							new Funcionario("Matheus", 
+								LocalDate.now(),
+								new BigDecimal("10000")));
+		
+		assertEquals(new BigDecimal("1000.00"), bonus);
+	}
+	
+}
